43dc53948e8cb185ac4cd763b3a7603bc65523f8
Simone Marino <simone.marino@usm.edu>
2024-05-02 10:08:52 -0500

    added styling, datatable, html

--- app.py
+++ app.py
@@ -6,17 +6,30 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:0774@localhost/Sc
 db = SQLAlchemy(app)
 
 @app.route('/')
-def index():
+def datatables():
     # Creiamo un motore SQLAlchemy con la stringa di connessione al tuo database
     engine = create_engine('postgresql://postgres:0774@localhost/ScholarData')
 
-    # Eseguiamo una query per ottenere i primi 10 record dal database
+    # Eseguiamo una query per ottenere tutti i record dal database
     with engine.connect() as connection:
         result = connection.execute(text("SELECT * FROM articles"))
         records = [dict(row._mapping) for row in result]
 
     # Passiamo i record alla nostra pagina HTML
-    return render_template('index.html', records=records)
+    return render_template('datatables.html', records=records)
+
+@app.route('/d3')
+def d3():
+    # Creiamo un motore SQLAlchemy con la stringa di connessione al tuo database
+    engine = create_engine('postgresql://postgres:0774@localhost/ScholarData')
+
+    # Eseguiamo una query per ottenere tutti i record dal database
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT * FROM articles"))
+        records = [dict(row._mapping) for row in result]
+
+    # Passiamo i record alla nostra pagina HTML
+    return render_template('d3.html', records=records)
 
 if __name__ == '__main__':
     app.run(debug=True)
